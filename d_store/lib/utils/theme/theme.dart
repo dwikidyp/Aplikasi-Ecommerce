cc038eac59ae1893be6ecb63327f872e3f0f7786
dwikidyp <dwiki834@gmail.com>
2024-03-03 21:15:59 +0700

    Daftar Putar

---
+++ d_store/lib/utils/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:d_store/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:d_store/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonThemeData.lightElevatedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
