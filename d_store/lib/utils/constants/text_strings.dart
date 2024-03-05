1cbfb00ade0dfade96b7fb7e58bf84766fc8726f
dwikidyp <dwiki834@gmail.com>
2024-03-05 21:41:17 +0700

    Teks Onboarding

---
+++ d_store/lib/utils/constants/text_strings.dart
@@ -0,0 +1,17 @@
+class TTexts {
+  // Onboarding
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 =
+      "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const String onBoardingSubTitle2 =
+      "Untuk Transaksi yang Lancar, Pilih Jalur Pembayaran Anda - Kenyamanan Anda, Prioritas Kami!";
+  static const String onBoardingSubTitle3 =
+      "Dari Depan Pintu Kami ke Rumah Anda - Pengiriman Cepat, Aman, dan Tanpa Kontak!";
+
+  // Home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Prince Dwiki";
+}
