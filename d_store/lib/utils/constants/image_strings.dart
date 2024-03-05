4746beef33a139a671c7235878ba75f8a2edb7d3
dwikidyp <dwiki834@gmail.com>
2024-03-05 19:22:34 +0700

    Formatters

---
+++ d_store/lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // App Logo
+  static const String darkAppLogo = "assets/logos/black.png";
+  static const String lightAppLogo = "assets/logos/white.png";
+
+  // Social Logo
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
