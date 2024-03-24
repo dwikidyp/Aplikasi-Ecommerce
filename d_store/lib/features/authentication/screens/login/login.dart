57c6e238fc3fc33f956e99331d888e62a1c12e26
dwikidyp <dwiki834@gmail.com>
2024-03-24 21:34:26 +0700

    Design Footer

--- d_store/lib/features/authentication/screens/login/login.dart
+++ d_store/lib/features/authentication/screens/login/login.dart
@@ -136,7 +136,31 @@ class LoginScreen extends StatelessWidget {
               /// Footer
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [Container()],
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: TColors.grey),
+                        borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                            width: TSizes.iconMd,
+                            height: TSizes.iconMd,
+                            image: AssetImage(TImages.google))),
+                  ),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: TColors.grey),
+                        borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                            width: TSizes.iconMd,
+                            height: TSizes.iconMd,
+                            image: AssetImage(TImages.facebook))),
+                  ),
+                ],
               )
             ],
           ),
