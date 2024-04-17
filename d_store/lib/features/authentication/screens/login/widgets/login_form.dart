8e3ddb073fa2a7e42d0b682cc4b689d4a1631222
dwikidyp <dwiki834@gmail.com>
2024-04-17 13:25:21 +0700

    Add Bottom Navigation bar

--- d_store/lib/features/authentication/screens/login/widgets/login_form.dart
+++ d_store/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:d_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:d_store/features/authentication/screens/signup/signup.dart';
+import 'package:d_store/navigation_menu.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:d_store/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -64,7 +65,8 @@ class TLoginForm extends StatelessWidget {
           SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                  onPressed: () {}, child: const Text(TTexts.signIn))),
+                  onPressed: () => Get.to(() => const NavigationMenu()),
+                  child: const Text(TTexts.signIn))),
           const SizedBox(height: TSizes.spaceBtwItems),
 
           /// Create Account
--- d_store/lib/navigation_menu.dart
+++ d_store/lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
@@ -7,8 +8,17 @@ class NavigationMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
-        destinations: [],
+        height: 80,
+        elevation: 0,
+        selectedIndex: 1,
+        destinations: const [
+          NavigationDestination(icon: Icon(Iconsax.home), label: 'home'),
+          NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+          NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+          NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+        ],
       ),
+      body: Container(),
     );
   }
 }
