c6563ac5372ca47ed45418fccd5a3311410d5ba7
dwikidyp <dwiki834@gmail.com>
2024-04-22 05:40:24 +0700

    Design Circular Container

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:d_store/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  TCircularContainer(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 400,
+      padding: const EdgeInsets.all(0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(400),
+          color: TColors.textWhite.withOpacity(0.1)),
+    );
   }
 }
