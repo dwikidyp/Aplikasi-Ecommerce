d83e4e555810f1da6fcc5dc2255643848e934d0a
dwikidyp <dwiki834@gmail.com>
2024-04-27 12:38:34 +0700

    Cihuyy

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:d_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:d_store/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:d_store/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -10,35 +11,26 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: -150,
-                      right: -250,
-                      child: TCircularContainer(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1),
-                      ),
-                    ),
-                    Positioned(
-                      top: 100,
-                      right: -300,
-                      child: TCircularContainer(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
+          children: [TCurvedEdgeWidget()],
         ),
       ),
+    );
+  }
+}
+
+class TCurvedEdgeWidget extends StatelessWidget {
+  const TCurvedEdgeWidget({
+    super.key,
+    this.child,
+  });
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: child,
     );
   }
 }
