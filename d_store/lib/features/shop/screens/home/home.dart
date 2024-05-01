7c2ca7eff61b9f22ac5085a3d40cc276a78e01a3
dwikidyp <dwiki834@gmail.com>
2024-05-01 16:45:12 +0700

    hehe

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:d_store/common/widgets/custom_shapes/containers/primary_header_c
 import 'package:d_store/utils/constants/colors.dart';
 import 'package:d_store/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,6 +19,7 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   TAppBar(
                     title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(TTexts.homeAppbarTitle,
                             style: Theme.of(context)
@@ -31,6 +33,11 @@ class HomeScreen extends StatelessWidget {
                                 .apply(color: TColors.white)),
                       ],
                     ),
+                    actions: [
+                      TCartCounterIcon(
+                        onPressed: () {},
+                      )
+                    ],
                   )
                 ],
               ),
@@ -38,6 +45,51 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class TCartCounterIcon extends StatelessWidget {
+  const TCartCounterIcon({
+    super.key,
+    required this.onPressed,
+    this.iconColor,
+  });
+
+  final VoidCallback onPressed;
+  final Color? iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+            onPressed: onPressed,
+            icon: Icon(
+              Iconsax.shopping_bag,
+              color: iconColor,
+            )),
+        Positioned(
+          right: 0,
+          child: Container(
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+              color: TColors.black.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Center(
+              child: Text(
+                '2',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .apply(color: TColors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
