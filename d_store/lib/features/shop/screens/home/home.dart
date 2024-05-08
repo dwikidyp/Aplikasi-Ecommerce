ad55fd141fd863a0ca34e8528eec900a482a9331
dwikidyp <dwiki834@gmail.com>
2024-05-08 15:47:16 +0700

    Create a circular image

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:d_store/common/widgets/custom_shapes/containers/search_container
 import 'package:d_store/common/widgets/texts/section_heading.dart';
 import 'package:d_store/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/utils/constants/image_strings.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
@@ -36,9 +37,10 @@ class HomeScreen extends StatelessWidget {
                     child: Column(
                       children: [
                         // Heading
-                        TSectionHeading(
+                        const TSectionHeading(
                           title: 'Popular Categories',
                           showActionButton: false,
+                          textColor: Colors.white,
                         ),
                         const SizedBox(
                           height: TSizes.spaceBtwItems,
@@ -52,22 +54,10 @@ class HomeScreen extends StatelessWidget {
                               itemCount: 6,
                               scrollDirection: Axis.horizontal,
                               itemBuilder: (_, index) {
-                                return Column(
-                                  children: [
-                                    Container(
-                                      width: 56,
-                                      height: 56,
-                                      padding: const EdgeInsets.all(TSizes.sm),
-                                      decoration: BoxDecoration(
-                                        color: TColors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(100),
-                                      ),
-                                      child: Center(
-                                        child: Image(image: AssetImage('')),
-                                      ),
-                                    )
-                                  ],
+                                return TVerticalImageText(
+                                  image: TImages.shoeIcon,
+                                  title: 'Shoes',
+                                  onTap: () {},
                                 );
                               }),
                         )
@@ -77,6 +67,67 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TVerticalImageText extends StatelessWidget {
+  const TVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.textColor = TColors.white,
+    this.backgroundColor,
+    this.onTap,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            // Circular Icon
+            Container(
+              width: 56,
+              height: 56,
+              padding: const EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                  color: TColors.dark,
+                ),
+              ),
+            ),
+            // Text
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            SizedBox(
+              width: 55,
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .apply(color: textColor),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            )
           ],
         ),
       ),
