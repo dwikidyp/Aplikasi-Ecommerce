b975ba486a570c3d4e033f429e9c1c3e901fc557
dwikidyp <dwiki834@gmail.com>
2024-05-09 19:13:38 +0700

    nambah banner promo

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -2,7 +2,7 @@ import 'package:d_store/common/widgets/custom_shapes/containers/primary_header_c
 import 'package:d_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:d_store/common/widgets/texts/section_heading.dart';
 import 'package:d_store/features/shop/screens/home/widgets/home_appbar.dart';
-import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:d_store/utils/constants/image_strings.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -16,117 +16,49 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
               child: Column(
                 children: [
                   ///Appbar
-                  const THomeAppBar(),
-                  const SizedBox(
+                  THomeAppBar(),
+                  SizedBox(
                     height: TSizes.spaceBtwSections,
                   ),
 
                   /// Searchbar
-                  const TSearchContainer(text: 'Search in Store'),
-                  const SizedBox(
+                  TSearchContainer(text: 'Search in Store'),
+                  SizedBox(
                     height: TSizes.spaceBtwSections,
                   ),
 
                   // Categories
                   Padding(
-                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
                     child: Column(
                       children: [
                         // Heading
-                        const TSectionHeading(
+                        TSectionHeading(
                           title: 'Popular Categories',
                           showActionButton: false,
                           textColor: Colors.white,
                         ),
-                        const SizedBox(
+                        SizedBox(
                           height: TSizes.spaceBtwItems,
                         ),
 
                         // Categories
-                        SizedBox(
-                          height: 80,
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: 6,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (_, index) {
-                                return TVerticalImageText(
-                                  image: TImages.shoeIcon,
-                                  title: 'Shoes',
-                                  onTap: () {},
-                                );
-                              }),
-                        )
+                        THomeCategories()
                       ],
                     ),
                   )
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TVerticalImageText extends StatelessWidget {
-  const TVerticalImageText({
-    super.key,
-    required this.image,
-    required this.title,
-    this.textColor = TColors.white,
-    this.backgroundColor,
-    this.onTap,
-  });
-
-  final String image, title;
-  final Color textColor;
-  final Color? backgroundColor;
-  final void Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
-        child: Column(
-          children: [
-            // Circular Icon
+            // Body
             Container(
-              width: 56,
-              height: 56,
-              padding: const EdgeInsets.all(TSizes.sm),
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Center(
-                child: Image(
-                  image: AssetImage(image),
-                  fit: BoxFit.cover,
-                  color: TColors.dark,
-                ),
-              ),
-            ),
-            // Text
-            const SizedBox(height: TSizes.spaceBtwItems / 2),
-            SizedBox(
-              width: 55,
-              child: Text(
-                title,
-                style: Theme.of(context)
-                    .textTheme
-                    .labelMedium!
-                    .apply(color: textColor),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(TSizes.md)),
+              child: const Image(image: AssetImage(TImages.promoBanner1)),
             )
           ],
         ),
--- d_store/lib/utils/constants/image_strings.dart
+++ d_store/lib/utils/constants/image_strings.dart
@@ -47,4 +47,9 @@ class TImages {
       "assets/icons/categories/icons8-sparkling-diamond-64.png";
   static const String electronicsIcon =
       "assets/icons/categories/icons8-smartphone-64.png";
+
+  // Banners
+  static const String promoBanner1 = "assets/images/banners/promo-banner-1.png";
+  static const String promoBanner2 = "assets/images/banners/promo-banner-2.png";
+  static const String promoBanner3 = "assets/images/banners/promo-banner-3.png";
 }
