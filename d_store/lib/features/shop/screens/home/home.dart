0762d84e0720ad4c71b57e9049fb6a248a084c2a
dwikidyp <dwiki834@gmail.com>
2024-05-12 05:39:16 +0700

    Mix

--- d_store/lib/features/shop/screens/home/home.dart
+++ d_store/lib/features/shop/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:d_store/common/widgets/custom_shapes/containers/search_container
 import 'package:d_store/common/widgets/texts/section_heading.dart';
 import 'package:d_store/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:d_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:d_store/utils/constants/colors.dart';
 import 'package:d_store/utils/constants/image_strings.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +13,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const TPrimaryHeaderContainer(
+            TPrimaryHeaderContainer(
               child: Column(
                 children: [
                   ///Appbar
@@ -55,13 +56,66 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             // Body
-            Container(
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(TSizes.md)),
-              child: const Image(image: AssetImage(TImages.promoBanner1)),
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: TRoundedImage(),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+    this.width = 150,
+    this.height = 158,
+    required this.imageUrl,
+    this.applyImageRadius = false,
+    this.border,
+    this.backgroundColor = TColors.light,
+    this.fit = BoxFit.contain,
+    this.padding,
+    this.isNetworkImage = false,
+    this.onPressed,
+    required this.borderRadius,
+  });
+
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(
+            border: border,
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(borderRadius)),
+        child: ClipRRect(
+            borderRadius: applyImageRadius
+                ? BorderRadius.circular(borderRadius)
+                : BorderRadius.zero,
+            child: Image(
+              image: isNetworkImage
+                  ? NetworkImage(imageUrl)
+                  : AssetImage(imageUrl) as ImageProvider,
+              fit: fit,
+            )),
       ),
     );
   }
