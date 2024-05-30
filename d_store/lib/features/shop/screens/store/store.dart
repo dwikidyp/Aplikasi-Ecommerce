fc23d0b896f65dec9024054cf080f8457d2668d1
dwikidyp <dwiki834@gmail.com>
2024-05-31 05:55:57 +0700

    add Featured Brands

--- d_store/lib/features/shop/screens/store/store.dart
+++ d_store/lib/features/shop/screens/store/store.dart
@@ -1,6 +1,10 @@
 import 'package:d_store/common/widgets/appbar/appbar.dart';
+import 'package:d_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:d_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:d_store/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:d_store/common/widgets/texts/section_heading.dart';
 import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/utils/constants/sizes.dart';
 import 'package:d_store/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
@@ -29,6 +33,33 @@ class StoreScreen extends StatelessWidget {
                     ? TColors.black
                     : TColors.white,
                 expandedHeight: 440,
+                flexibleSpace: Padding(
+                  padding: const EdgeInsets.all(TSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      /// Search bar
+                      const SizedBox(height: TSizes.spaceBtwItems),
+                      const TSearchContainer(
+                        text: 'Search in Store',
+                        showBorder: true,
+                        showBackground: false,
+                        padding: EdgeInsets.zero,
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      /// featured Brands
+                      TSectionHeading(
+                          title: 'Featured Brands', onPressed: () {}),
+                      const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                      const TRoundedContainer(
+                        padding: EdgeInsets.all(TSizes.sm),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ];
           },
