dd5d72057e1adbb872279746b32b56f8b89849c4
dwikidyp <dwiki834@gmail.com>
2024-06-07 07:17:13 +0700

    body store

---
+++ d_store/lib/common/widgets/appbar/tabbar.dart
@@ -0,0 +1,30 @@
+import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/utils/device/device_utility.dart';
+import 'package:d_store/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TTabBar extends StatelessWidget implements PreferredSizeWidget {
+  /// If You Want
+  /// To do that [PreferredSize] Widget and that's [PreferredSizeWidget]
+  const TTabBar({super.key, required this.tabs});
+
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Material(
+      color: dark ? TColors.black : TColors.white,
+      child: TabBar(
+        tabs: tabs,
+        isScrollable: true,
+        indicatorColor: TColors.primary,
+        labelColor: dark ? TColors.white : TColors.primary,
+        unselectedLabelColor: TColors.darkGrey,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
--- d_store/lib/features/shop/screens/store/store.dart
+++ d_store/lib/features/shop/screens/store/store.dart
@@ -1,4 +1,5 @@
 import 'package:d_store/common/widgets/appbar/appbar.dart';
+import 'package:d_store/common/widgets/appbar/tabbar.dart';
 import 'package:d_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:d_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:d_store/common/widgets/images/circular_image.dart';
@@ -18,114 +19,113 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TAppBar(
-        title: Text('Store', style: Theme.of(context).textTheme.headlineMedium),
-        actions: [
-          TCartCounterIcon(
-            onPressed: () {},
-            iconColor: TColors.white,
-          ),
-        ],
-      ),
-      body: NestedScrollView(
-          headerSliverBuilder: (_, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                pinned: true,
-                floating: true,
-                backgroundColor: THelperFunctions.isDarkMode(context)
-                    ? TColors.black
-                    : TColors.white,
-                expandedHeight: 440,
-                flexibleSpace: Padding(
-                  padding: const EdgeInsets.all(TSizes.defaultSpace),
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      /// Search bar
-                      const SizedBox(height: TSizes.spaceBtwItems),
-                      const TSearchContainer(
-                        text: 'Search in Store',
-                        showBorder: true,
-                        showBackground: false,
-                        padding: EdgeInsets.zero,
-                      ),
-                      const SizedBox(height: TSizes.spaceBtwSections),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: TAppBar(
+          title:
+              Text('Store', style: Theme.of(context).textTheme.headlineMedium),
+          actions: [
+            TCartCounterIcon(
+              onPressed: () {},
+              iconColor: TColors.white,
+            ),
+          ],
+        ),
+        body: NestedScrollView(
+            headerSliverBuilder: (_, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: THelperFunctions.isDarkMode(context)
+                      ? TColors.black
+                      : TColors.white,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                    padding: const EdgeInsets.all(TSizes.defaultSpace),
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        /// Search bar
+                        const SizedBox(height: TSizes.spaceBtwItems),
+                        const TSearchContainer(
+                          text: 'Search in Store',
+                          showBorder: true,
+                          showBackground: false,
+                          padding: EdgeInsets.zero,
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwSections),
 
-                      /// featured Brands
-                      TSectionHeading(
-                          title: 'Featured Brands', onPressed: () {}),
-                      const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+                        /// featured Brands
+                        TSectionHeading(
+                            title: 'Featured Brands', onPressed: () {}),
+                        const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
-                      /// Brands Grid
-                      TGridLayout(
-                          itemCount: 4,
-                          mainAxisExtent: 80,
-                          itemBuilder: (_, index) {
-                            return GestureDetector(
-                              onTap: () {},
-                              child: TRoundedContainer(
-                                padding: const EdgeInsets.all(TSizes.sm),
-                                showBorder: true,
-                                backgroundColor: Colors.transparent,
-                                child: Row(
-                                  children: [
-                                    /// Icon
-                                    Flexible(
-                                      child: TCircularImage(
-                                        image: TImages.clothIcon,
-                                        isNetworkImage: false,
-                                        backgroundColor: Colors.transparent,
-                                        overlayColor:
-                                            THelperFunctions.isDarkMode(context)
-                                                ? TColors.white
-                                                : TColors.black,
+                        /// Brands Grid
+                        TGridLayout(
+                            itemCount: 4,
+                            mainAxisExtent: 80,
+                            itemBuilder: (_, index) {
+                              return GestureDetector(
+                                onTap: () {},
+                                child: TRoundedContainer(
+                                  padding: const EdgeInsets.all(TSizes.sm),
+                                  showBorder: true,
+                                  backgroundColor: Colors.transparent,
+                                  child: Row(
+                                    children: [
+                                      /// Icon
+                                      Flexible(
+                                        child: TCircularImage(
+                                          image: TImages.clothIcon,
+                                          isNetworkImage: false,
+                                          backgroundColor: Colors.transparent,
+                                          overlayColor:
+                                              THelperFunctions.isDarkMode(
+                                                      context)
+                                                  ? TColors.white
+                                                  : TColors.black,
+                                        ),
                                       ),
-                                    ),
-                                    const SizedBox(
-                                        width: TSizes.spaceBtwItems / 2),
+                                      const SizedBox(
+                                          width: TSizes.spaceBtwItems / 2),
 
-                                    /// Text
-                                    Expanded(
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const TBrandTitleWithVerifiedIcon(
-                                            title: 'Adidas',
-                                            brandTextSize: TextSizes.large,
-                                          ),
-                                          Text(
-                                            '256 products with asdwaagf',
-                                            overflow: TextOverflow.ellipsis,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelMedium,
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
+                                      /// Text
+                                      Expanded(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            const TBrandTitleWithVerifiedIcon(
+                                              title: 'Adidas',
+                                              brandTextSize: TextSizes.large,
+                                            ),
+                                            Text(
+                                              '256 products with asdwaagf',
+                                              overflow: TextOverflow.ellipsis,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelMedium,
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            );
-                          }),
-                    ],
+                              );
+                            }),
+                      ],
+                    ),
                   ),
-                ),
 
-                /// Tabs
-                bottom: TabBar(
-                    isScrollable: true,
-                    indicatorColor: TColors.primary,
-                    labelColor: THelperFunctions.isDarkMode(context)
-                        ? TColors.white
-                        : TColors.primary,
-                    tabs: const [
+                  /// Tabs
+                  bottom: const TTabBar(
+                    tabs: [
                       Tab(
                         child: Text('Sports'),
                       ),
@@ -141,11 +141,31 @@ class StoreScreen extends StatelessWidget {
                       Tab(
                         child: Text('Cosmetics'),
                       ),
-                    ]),
-              ),
-            ];
-          },
-          body: Container()),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: const TabBarView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      /// Brands
+                      TRoundedContainer(
+                        showBorder: true,
+                        borderColor: TColors.darkGrey,
+                        backgroundColor: Colors.transparent,
+                        margin: EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+                        child: Column(),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
