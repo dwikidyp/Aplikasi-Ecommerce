fa2f3aea59291054976a1027a267449ebb50dd27
dwikidyp <dwiki834@gmail.com>
2024-06-25 11:22:41 +0700

    Design User Detail

--- d_store/lib/features/personalization/screens/profile/profile.dart
+++ d_store/lib/features/personalization/screens/profile/profile.dart
@@ -1,8 +1,11 @@
 import 'package:d_store/common/widgets/appbar/appbar.dart';
 import 'package:d_store/common/widgets/images/circular_image.dart';
+import 'package:d_store/common/widgets/texts/section_heading.dart';
+import 'package:d_store/features/personalization/screens/profile/widgets/profile_menu.dart';
 import 'package:d_store/utils/constants/image_strings.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -42,6 +45,55 @@ class ProfileScreen extends StatelessWidget {
               const SizedBox(height: TSizes.spaceBtwItems / 2),
               const Divider(),
               const SizedBox(height: TSizes.spaceBtwItems),
+
+              /// Heading Profile
+              const TSectionHeading(
+                  title: 'Profile Information', showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              TProfileMenu(
+                  onPressed: () {}, title: 'Name', value: 'Prince Dwiki'),
+              TProfileMenu(
+                  onPressed: () {}, title: 'Username', value: 'prince_dwiki'),
+
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const Divider(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              /// Heading Presonal Info
+              const TSectionHeading(
+                  title: 'Personal Information', showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              TProfileMenu(
+                onPressed: () {},
+                title: 'User ID',
+                value: '123',
+                icon: Iconsax.copy,
+              ),
+              TProfileMenu(onPressed: () {}, title: 'E-mail', value: 'dwiki'),
+              TProfileMenu(
+                  onPressed: () {},
+                  title: 'Phone Number',
+                  value: '+628139874187'),
+              TProfileMenu(onPressed: () {}, title: 'Gender', value: 'Male'),
+              TProfileMenu(
+                  onPressed: () {},
+                  title: 'Date Of Birth',
+                  value: '6 Jul, 2003'),
+
+              const Divider(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Close Account',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              )
             ],
           ),
         ),
