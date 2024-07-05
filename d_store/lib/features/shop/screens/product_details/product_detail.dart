dcf027f5ea65e6588959af93ae749f47bc2d2a66
dwikidyp <dwiki834@gmail.com>
2024-07-06 05:20:12 +0700

    Create Product Detail UI

--- d_store/lib/features/shop/screens/product_details/product_detail.dart
+++ d_store/lib/features/shop/screens/product_details/product_detail.dart
@@ -1,4 +1,7 @@
 import 'package:d_store/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:d_store/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:d_store/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:d_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -14,6 +17,21 @@ class ProductDetailScreen extends StatelessWidget {
             TProductImageSlider(),
 
             /// Product Detail
+            Padding(
+              padding: EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// Rating & Share
+                  TRatingAndShare(),
+
+                  /// Price, Title, Stock & Brand
+                  TProductMetaData(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
---
+++ d_store/lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -0,0 +1,45 @@
+import 'package:d_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/utils/constants/sizes.dart';
+import 'package:d_store/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TProductMetaData extends StatelessWidget {
+  const TProductMetaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        /// Price & Sale Price
+        Row(
+          children: [
+            /// Sale Tag
+            TRoundedContainer(
+              radius: TSizes.sm,
+              backgroundColor: TColors.secondary.withOpacity(0.8),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: TSizes.sm, vertical: TSizes.xs),
+              child: Text(
+                '25%',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .apply(color: TColors.black),
+              ),
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems),
+
+            /// Price
+            Text(
+              '\RP11.000',
+              style: Theme.of(context).textTheme.titleSmall!,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
