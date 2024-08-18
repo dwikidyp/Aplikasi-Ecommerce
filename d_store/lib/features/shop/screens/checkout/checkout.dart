0c712f782fa9670a569cdbf89b658173faf58b2f
dwikidyp <dwiki834@gmail.com>
2024-08-18 18:56:10 +0700

    BILLING payment section

--- d_store/lib/features/shop/screens/checkout/checkout.dart
+++ d_store/lib/features/shop/screens/checkout/checkout.dart
@@ -1,6 +1,8 @@
 import 'package:d_store/common/widgets/appbar/appbar.dart';
 import 'package:d_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:d_store/common/widgets/products/cart/coupon_widget.dart';
 import 'package:d_store/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:d_store/features/shop/screens/checkout/widgets/billing_payment_section.dart';
 import 'package:d_store/utils/constants/colors.dart';
 import 'package:d_store/utils/constants/sizes.dart';
 import 'package:d_store/utils/helpers/helper_functions.dart';
@@ -30,27 +32,26 @@ class CheckoutScreen extends StatelessWidget {
               const SizedBox(height: TSizes.spaceBtwSections),
 
               /// Coupon TextFIeld
+              const TCouponCode(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Billing
               TRoundedContainer(
                 showBorder: true,
-                backgroundColor: dark ? TColors.dark : TColors.white,
-                padding: const EdgeInsets.only(
-                    top: TSizes.sm,
-                    bottom: TSizes.sm,
-                    right: TSizes.sm,
-                    left: TSizes.md),
-                child: Row(
+                padding: const EdgeInsets.all(TSizes.md),
+                backgroundColor: dark ? TColors.black : TColors.white,
+                child: const Column(
                   children: [
-                    Flexible(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: 'Have a promo code enter here',
-                            focusedBorder: InputBorder.none),
-                      ),
-                    ),
+                    /// Pricing
+                    TBillingPaymentSection(),
+                    SizedBox(height: TSizes.spaceBtwItems),
+
+                    /// Divider
+                    Divider(),
+                    SizedBox(height: TSizes.spaceBtwItems),
                   ],
                 ),
-              ),
-              const SizedBox(height: TSizes.spaceBtwSections),
+              )
             ],
           ),
         ),
---
+++ d_store/lib/features/shop/screens/checkout/widgets/billing_payment_section.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TBillingPaymentSection extends StatelessWidget {
+  const TBillingPaymentSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Subtotal
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Subtotal', style: Theme.of(context).textTheme.bodyMedium),
+            Text('\RP 4000', style: Theme.of(context).textTheme.bodyMedium),
+          ],
+        )
+      ],
+    );
+  }
+}
