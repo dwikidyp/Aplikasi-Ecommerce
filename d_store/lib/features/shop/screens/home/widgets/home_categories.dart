eaed086e646f4f9b4273643a3c507d2340b5ab23
dwikidyp <dwiki834@gmail.com>
2024-08-29 18:54:16 +0700

    sub categories

--- d_store/lib/features/shop/screens/home/widgets/home_categories.dart
+++ d_store/lib/features/shop/screens/home/widgets/home_categories.dart
@@ -1,6 +1,8 @@
 import 'package:d_store/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:d_store/features/shop/screens/sub_category/sub_categories.dart';
 import 'package:d_store/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class THomeCategories extends StatelessWidget {
   const THomeCategories({
@@ -19,7 +21,7 @@ class THomeCategories extends StatelessWidget {
             return TVerticalImageText(
               image: TImages.shoeIcon,
               title: 'Shoes',
-              onTap: () {},
+              onTap: () => Get.to(() => const SubCategoriesScreen()),
             );
           }),
     );
---
+++ d_store/lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,48 @@
+import 'package:d_store/common/widgets/appbar/appbar.dart';
+import 'package:d_store/common/widgets/images/t_rounded_image.dart';
+import 'package:d_store/common/widgets/texts/section_heading.dart';
+import 'package:d_store/utils/constants/image_strings.dart';
+import 'package:d_store/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TAppBar(
+        title: Text('Sports'),
+        showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// banner
+              const TRoundedImage(
+                width: double.infinity,
+                imageUrl: TImages.promoBanner2,
+                applyImageRadius: true,
+              ),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Sub-Categories
+              Column(
+                children: [
+                  /// Heading
+                  TSectionHeading(
+                    title: 'Sports Shirs',
+                    onPressed: () {},
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems / 2),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
